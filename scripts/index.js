d7fe9a15b8575b363121a8a2257e3c0470861644
shori <deadsher@mail.ru>
2023-03-03 02:05:43 +0500

    Редактирует попап

--- scripts/index.js
+++ scripts/index.js
@@ -13,3 +13,71 @@ popupClose.addEventListener('click', function(e){
     e.preventDefault();
     popup.classList.remove('active');
 })
+
+
+// Даты попапа
+
+function getAvailability(dateInput) {
+    let answer;
+    const day = new Date().getDate();
+    const month = new Date().getMonth();
+    const year = new Date().getFullYear();
+    const currentDate = [day, month, year];
+    let userDate = dateInput.split(' ');
+    let Months = [
+        'январ',
+        'феврал',
+        'март',
+        'апрел',
+        'ма',
+        'июн',
+        'июл',
+        'август',
+        'сентябр',
+        'ноябр',
+        'декабр',
+    ];
+    userDate = [
+        parseInt(userDate[0], 10),
+        Months.indexOf(userDate[1].slice(0, -1)) + 1,
+        parseInt(userDate[2], 10)
+    ];
+
+    answer = "succes"
+    for (let i = 2; i >= 0; i--) {
+        if (currentDate[i] > userDate[i]) {
+            answer = "dateMismatch";
+            break
+        }
+    }
+
+    return answer;
+}
+
+function createMessage(answer) {
+    let message;
+
+    if (answer == "succes") {
+      message = "На эти даты есть свободные номера. Пока есть.";
+    } else if (answer == "dateMismatch") {
+      message = "Мы не можем отправить вас в прошлое.";
+    }
+
+    return message;
+}
+
+
+const firstDate = document.getElementById("#firstDate");
+const secondDate = document.getElementById("#secondDate");
+const firstDateState = document.getElementById("#firstDateState");
+const secondDateState = document.getElementById("secondDateState");
+
+firstDate.onchange = function() {
+    let firstDateMessage = createMessage(getAvailability(firstDate.value));
+    firstDateState.value = firstDateMessage;
+}
+
+secondDate.onchange = function() {
+    let secondDateMessage = createMessage(getAvailability(secondDate.value));
+    secondDateState.value = secondDateMessage;
+}
